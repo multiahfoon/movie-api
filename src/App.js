025767b73b97af44302deda967121b86b5653ea0
multi <multiahfoon@gmail.com>
2021-03-15 16:43:07 +1300

    feat: add useEffect to make api call using axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import axios from 'axios';
+import React, { useEffect } from 'react'
+
 import './App.css';
 import Header from './components/ui/Header'
 function App() {
+  // TODO: needs refactoring, code smells
+  useEffect(() => {
+    const fetchMovie = async () => {
+      const result = await axios (`http://www.omdbapi.com/?i=tt3896198&apikey=${process.env.react_app_apikey}`)
+      console.log(result.data)
+    }
+    fetchMovie()
+  }, [])
+
+
   return (
     <div className="container">
       <Header />
