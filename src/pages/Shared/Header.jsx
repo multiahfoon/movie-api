4a9eb6343b673267353757f5fbb170daae4dd342
multi <multiahfoon@gmail.com>
2021-03-20 18:59:38 +1300

    build: directory restructure
    note: This is an to make component directory more meaningful when reading. This may change in the near future.

---
+++ src/pages/Shared/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <h1>Hello World</h1>
+        <nav>
+          <input type="text"/>
+          <button>Search</button>
+        </nav>
+      </header>
+    </>
+  )
+}
+
+export default Header
