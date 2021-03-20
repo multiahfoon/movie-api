855adff2803cd0f8810ed7c62e1a495734630f91
multi <multiahfoon@gmail.com>
2021-03-20 22:10:49 +1300

    feat(home): render movie posters from api
    To get access to the images from api, a base url is needed.
    I'm not completely sure, but it seems the base url is and open endpoint.

--- src/pages/Home/MovieCard.jsx
+++ src/pages/Home/MovieCard.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
+// TODO: Need to move this else where, maybe into consume file
+// Need a better way of handling consume
+const baseUrl = 'https://image.tmdb.org/t/p/w500/'
+
+
 const MovieCard = ({data}) => {
   return (
     <>
       <div className="movieCard">
-        <p>{data.title}</p>
+        <img className="poster" src={baseUrl + data.poster_path} alt=""/>
       </div>
     </>
   )
