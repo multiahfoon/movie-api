dc6f0d0ed515d4d7507df01503126d40c000a23f
multi <multiahfoon@gmail.com>
2021-03-20 16:35:48 +1300

    feat: add footer/nav

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import axios from 'axios';
+import axios from 'axios'
 import React, { useEffect } from 'react'
 
-import './App.css';
+import './App.css'
 import Header from './components/ui/Header'
+import Footer from './components/ui/Footer'
+
 function App() {
   // TODO: needs refactoring, code smells
   useEffect(() => {
     // this can be added in a consume folder
     const fetchMovie = async () => {
-      const result = await axios (`http://www.omdbapi.com/?i=tt3896198&apikey=${process.env.react_app_apikey}`)
+      const result = await axios(`http://www.omdbapi.com/?i=tt3896198&apikey=${process.env.react_app_apikey}`)
       console.log(result.data)
     }
     fetchMovie()
@@ -18,9 +20,9 @@ function App() {
   return (
     <div className="container">
       <Header />
-      
+      <Footer />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/ui/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <h1>Hello World</h1>
+        <nav>
+            <ul>
+              <li>Linkedin</li>
+              <li>Github</li>
+              <li>Portfolio</li>
+            </ul>
+        </nav>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
