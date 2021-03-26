3665f18a51e4b8b709400e2b08ae73c5f245b4f7
multi <multiahfoon@gmail.com>
2021-03-26 23:30:23 +1300

    refactor(api): moved dispatched actions around for loading time
    This has yet to be tested to see if there are any differences in
    load time.

--- src/api/theMovieDb.js
+++ src/api/theMovieDb.js
@@ -8,9 +8,9 @@ const clear = []
 export function getPopularMovies () {
   axios(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`)
     .then(movies => {
-      dispatch(popularMovies(movies.data.results))
       dispatch(searchMovie(clear))
       dispatch(movieDetails(clear))
+      dispatch(popularMovies(movies.data.results))
       return null
     })
     .catch(err => console.error(err))
@@ -19,8 +19,8 @@ export function getPopularMovies () {
 export function getSearchMovie (movie) {
   axios(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${movie}&language=en-US&page=1&include_adult=false`)
     .then(movieFound => {
-      dispatch(searchMovie(movieFound.data.results))
       dispatch(popularMovies(clear))
+      dispatch(searchMovie(movieFound.data.results))
     })
     .catch(err => console.error(err))
 }
@@ -29,9 +29,9 @@ export function getSearchMovie (movie) {
 export function getMovieDetails (id) {
   axios(`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&language=en-US`)
     .then(details => {
-      dispatch(movieDetails(details.data))
       dispatch(searchMovie(clear))
       dispatch(popularMovies(clear))
+      dispatch(movieDetails(details.data))
     })
     .catch(err => console.error(err))
 }
