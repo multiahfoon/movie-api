2690309db7f92c94e8444b7e29b0ad9741912a52
multi <multiahfoon@gmail.com>
2021-03-26 20:18:51 +1300

    feat(movie cards): movie cards redirect to movie page when clicked

--- src/pages/Home/MovieCard.jsx
+++ src/pages/Home/MovieCard.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const baseImgUrl = 'https://image.tmdb.org/t/p/w500/'
 
 // TODO: add backup for when there is no poster image
-const MovieCard = ({data}) => {
+const MovieCard = ({ data }) => {
   return (
     <>
       <div className="movieCard">
-        <img className="poster" src={baseImgUrl + data.poster_path} alt=""/>
+        <Link to={'/movie'}>
+          <img className="poster" src={baseImgUrl + data.poster_path} alt="" />
+        </Link>
       </div>
     </>
   )
