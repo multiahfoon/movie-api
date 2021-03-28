4bd4b309cc5854fc2411d5e8b40021dc766e0c92
multi <multiahfoon@gmail.com>
2021-03-29 12:15:56 +1300

    style(header/nav): clean up syntax and indenting
    Small syntax and indenting clean up, hopefully makes it a little
    bit easier to read.

--- src/pages/Shared/Header.jsx
+++ src/pages/Shared/Header.jsx
@@ -6,40 +6,34 @@ import { getSearchMovie, getPopularMovies } from '../../api/theMovieDb'
 
 const Header = (props) => {
   const [search, setSearch] = useState('')
-
-  const handleChange = (e) => {
-    setSearch(e.target.value)
-  }
-
+  const handleChange = (e) => setSearch(e.target.value)
+  
   const handleSubmit = (e) => {
     e.preventDefault()
-    
-    // if search is an empty string do not make api call
-    return search !== "" 
-      ? getSearchMovie(search) 
-      : null
+    // if search is an empty do not make api call
+    return search !== "" ? getSearchMovie(search) : null
   }
 
-  const resetMovies = () => {
-    getPopularMovies()
-  }
+const resetMovies = () => {
+  getPopularMovies()
+}
 
-  return (
-    <>
-      <header>
-        <Link to='/' onClick={resetMovies}>
-          <h1>Hello World</h1>
-        </Link>
-        <nav>
-          <form onSubmit={handleSubmit}>
-            <input type="text" onChange={handleChange} />
-            <button>Search</button>
-          </form>
-        </nav>
+return (
+  <>
+    <header>
+      <Link to='/' onClick={resetMovies}>
+        <h1>Hello World</h1>
+      </Link>
+      <nav>
+        <form onSubmit={handleSubmit}>
+          <input type="text" onChange={handleChange} />
+          <button>Search</button>
+        </form>
+      </nav>
 
-      </header>
-    </>
-  )
+    </header>
+  </>
+)
 }
 
 export default Header
