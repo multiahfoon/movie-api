2b9ac1305114f533b3e1aadd4e8cd5c814f45f65
multi <multiahfoon@gmail.com>
2021-03-29 15:56:39 +1300

    feat(movie details): clear store when movie details are rendered
    I don't know if this is best practice but I chose to clear the
    other states in store so it's easier work with what I need
    depending on what page I'm on.

--- src/pages/Movie/MovieDetails.jsx
+++ src/pages/Movie/MovieDetails.jsx
@@ -4,31 +4,36 @@ import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { useParams } from 'react-router'
 
+import { dispatch } from '../../store'
+import { searchMovie, popularMovies } from '../../actions'
+
 // fires request to api to get movie and dispatches action
 import { getMovieDetails } from '../../api/theMovieDb'
 
 const MovieDetails = (props) => {
   // const Id = '399566'
-  const { id }  = useParams()
+  const { id } = useParams()
   const { movie } = props
-
+  
   useEffect(() => {
     getMovieDetails(id)
+    dispatch(searchMovie([]))
+    dispatch(popularMovies([]))
   }, [id])
+
 
   // TODO: this works for now but needs refactoring  
   return movie.length < 1 ?
-   null 
-   : (
-    <>
-      <h1>Movie Details</h1>
-      
-      <h2>{movie.title}</h2>
-    </>
-  )
+    null
+    : (
+      <>
+        <h1>Movie Details</h1>
+        <h2>{movie.title}</h2>
+      </>
+    )
 }
 
-function mapStateToProp (state) {
+function mapStateToProp(state) {
   return {
     movie: state.movieDetails
   }
