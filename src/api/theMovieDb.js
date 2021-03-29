7bd207ea3be1c251a847f6be5823d19c3482eb7e
multi <multiahfoon@gmail.com>
2021-03-29 15:45:47 +1300

    refactor(api): remove action that clears state
    Api was doing more than it should. Removed dispatchs the were
    reseting individual states in store.

--- src/api/theMovieDb.js
+++ src/api/theMovieDb.js
@@ -4,12 +4,9 @@ import { dispatch } from '../store'
 import { popularMovies, searchMovie, movieDetails } from '../actions'
 
 const apiKey = process.env.react_app_apikey
-const clear = []
 export function getPopularMovies() {
   axios(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`)
     .then(movies => {
-      dispatch(searchMovie(clear))
-      dispatch(movieDetails(clear))
       dispatch(popularMovies(movies.data.results))
       return null
     })
@@ -19,8 +16,6 @@ export function getPopularMovies() {
 export function getSearchMovie(movie) {
   axios(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${movie}&language=en-US&page=1&include_adult=false`)
     .then(movieFound => {
-      dispatch(popularMovies(clear))
-      dispatch(movieDetails(clear))
       dispatch(searchMovie(movieFound.data.results))
     })
     .catch(err => console.error(err))
@@ -30,8 +25,6 @@ export function getSearchMovie(movie) {
 export function getMovieDetails(id) {
   axios(`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&language=en-US`)
     .then(details => {
-      dispatch(searchMovie(clear))
-      dispatch(popularMovies(clear))
       dispatch(movieDetails(details.data))
     })
     .catch(err => console.error(err))
