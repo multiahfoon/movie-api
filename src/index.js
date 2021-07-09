586e17ec615b33a571f136e81a7aeaa54cd2f5e8
multiahfoon <multiahfoon@gmail.com>
2021-07-09 12:25:52 +1200

    style: clean up import order

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-// react related
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom'
-import App from './App';
 
-// redux
-import store from './store'
 import { Provider } from 'react-redux'
+import store from './store'
+
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
