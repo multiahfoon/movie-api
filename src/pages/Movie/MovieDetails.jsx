5c446db0b425131910fc07e1ed89c44864b8b2d2
multiahfoon <multiahfoon@gmail.com>
2021-07-09 12:19:40 +1200

    style: clean up comments and syntax

--- src/pages/Movie/MovieDetails.jsx
+++ src/pages/Movie/MovieDetails.jsx
@@ -1,43 +1,39 @@
 import React, { useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
-
-// used to connect to redux store
-import { connect } from 'react-redux'
 import { useParams } from 'react-router'
+
+import { connect } from 'react-redux'
 import { dispatch } from '../../store'
-import './MovieDetails.styles.css'
 import { searchMovie, movieDetails } from '../../actions'
 
 
-// fires request to api to get movie and dispatches action
+import './MovieDetails.styles.css'
 import { getMovieDetails, getPopularMovies } from '../../api/theMovieDb'
-
-
 const basePosterUrl = 'https://image.tmdb.org/t/p/w500'
 
 const MovieDetails = (props) => {
   const { id } = useParams()
   const { movie } = props
+  const history = useHistory()
+  const clear = []
 
   useEffect(() => {
     getMovieDetails(id)
   }, [id])
 
-  const history = useHistory()
   const handleClick = () => {
-    dispatch(searchMovie([]))
-    dispatch(movieDetails([]))
+    dispatch(searchMovie(clear))
+    dispatch(movieDetails(clear))
     getPopularMovies()
     history.push('/')
   }
 
-  // stops markup from rendering until movies has completely loaded 
+  // stops markup from rendering until state in store has completely loaded 
   return movie.length < 1 ?
     null
     : (
       <>
         <section className='movieDetails' >
-
           <div className="detailsPoster">
             {
               movie.poster_path !== null
@@ -69,7 +65,6 @@ const MovieDetails = (props) => {
               <button onClick={handleClick}>Back</button>
             </div>
           </article>
-
         </section>
       </>
     )
